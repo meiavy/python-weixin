3bd1384575674cbe28cf784f0ecad7d68f87684a
meiavy <meiavy@163.com>
2016-06-29 14:54:45 +0800

    a

--- weixin.py
+++ weixin.py
@@ -1,5 +1,6 @@
 import web
 import time
+import xml.etree.ElementTree
 urls=(
 '/','index'
 )
@@ -9,14 +10,34 @@ class index:
   def GET(self):
     print web.input()
     params=web.input()
-    signature=params.signature
-    echostr=params.echostr
+    signature=params.signature if hasattr(params, "signature") else ""
+    echostr=params.echostr if hasattr(params, "echostr") else ""
     return echostr
   def POST(self):
     print web.input()
     print web.data()
     params=web.input()
-    openid=params.openid
+    openid=params.openid if hasattr(params, "openid") else ""
+    data=web.data()
+
+    root = xml.etree.ElementTree.XML(data).getroot()
+
+    print root.findall("ToUserName")
+    print root.findall("./ToUserName")
+    print root.findall("/xml/ToUserName")
+    print root.findall("//ToUserName")
+    #xmlRoot
+
+    """<xml><ToUserName><![CDATA[gh_a15e2f8bae8b]]></ToUserName>
+    <FromUserName><![CDATA[oJtIMsxje0-7rt0S18WH2iIhkMNU]]></FromUserName>
+    <CreateTime>1467180642</CreateTime>
+    <MsgType><![CDATA[text]]></MsgType>
+    <Content><![CDATA[111dsfdsafsafasfd]]></Content>
+    <MsgId>6301492875129622501</MsgId>
+    </xml>"""
+
+
+
     str="""<xml>
     <ToUserName><![CDATA["""
     str+=openid
